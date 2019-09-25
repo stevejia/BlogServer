d3f964ec13f5f6db4aaf8cbf25a9cf408dd571a6
Steve.Jia <1102263474@qq.com>
2019-09-25 11:31:39 +0800

    revert commit

--- db/articleTypeModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const articleTypeSchema = new Schema({
-    text: String,//显示文本
-    value: Number//值
-},{
-    collection: "ArticleTypes"
-});
-module.exports = exports = articleTypeSchema
